14992aa3cd18de15e7ee9ad969bdb6d547732dec
luisquintanilla <Luis.Quintanilla@microsoft.com>
2025-07-22 15:41:11 -0400

    Initial commit

---
+++ AspireN8N.AppHost/AppHost.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+
+builder.AddContainer("n8n", "n8nio/n8n")
+    .WithVolume("n8n_data", "/home/node/.n8n")
+    .WithHttpEndpoint(5678, 5678, "n8n");
+
+builder.Build().Run();
